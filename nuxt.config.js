716edbc845b90e62ae885025c669c68a856cabbf
Felipe Lopes <felipemarcanthlopes@gmail.com>
2024-01-10 16:03:03 -0300

    ajuste

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,15 +76,15 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    //baseURL: '/laravel',
-    baseURL: "http://cassino.fmlsoftware.com.br",
+    baseURL: '/laravel',
+    //baseURL: "http://cassino.fmlsoftware.com.br",
     credentials: true,
     proxy: false,
   },
 
   proxy: {
     '/laravel': {
-      target: "https://api.jogosbr.bet",
+      target: "http://cassino.fmlsoftware.com.br",
       pathRewrite: { '^/laravel': '' }
     }
   },
