f0246c172d72b738d744c3ed11b1d9bfe0658e9f
Felipe Lopes <felipemarcanthlopes@gmail.com>
2024-01-10 15:59:46 -0300

    ajuste

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,7 +77,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     //baseURL: '/laravel',
-    baseURL: "https://api.jogosbr.bet",
+    baseURL: "http://cassino.fmlsoftware.com.br",
     credentials: true,
     proxy: false,
   },
