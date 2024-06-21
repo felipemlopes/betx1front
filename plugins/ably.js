0dab0c8d09df4faf6af0c941c55bc995d33594da
Felipe Lopes <felipemarcanthlopes@gmail.com>
2024-06-21 01:35:59 -0300

    ajustes

--- plugins/ably.js
+++ plugins/ably.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Ably from 'ably';
 export default ({}, inject) => {
   let ably = new Ably.Realtime({
-    key:process.env.ABLY,
+    key:"Qd259w.d97xeg:fYaqLTQWIpCQ7ppgLENwCa8BC1k78VmWfgc8MlIasXs",
   });
   inject('ably', ably)
 }
